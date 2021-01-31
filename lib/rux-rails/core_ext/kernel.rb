373f8a0c4173ff2fe7dad7f5845296d0a20e1568
Cameron Dutro <camertron@gmail.com>
2021-01-31 10:08:06 -0800

    Get .rb file extensions working for ActiveSupport::Dependencies

--- lib/rux-rails/core_ext/kernel.rb
+++ lib/rux-rails/core_ext/kernel.rb
@@ -19,45 +19,39 @@ module RuxRails
 
   module RequirePatch
     def require(file)
-      begin
-        # try to require the file as normal
-        super(file)
-      rescue LoadError => e
-        # normal require failed, so check to see if there's a .rux file
-        # somewhere on the load path
-        path = nil
-        rux_file = file.end_with?('.rux') ? file : "#{file}.rux"
+      # check to see if there's a .rux file somewhere on the load path
+      path = nil
+      rux_file = file.end_with?('.rux') ? file : "#{file}.rux"
 
-        if File.absolute_path(rux_file) == rux_file && File.exist?(rux_file)
-          path = rux_file
-        elsif rux_file.start_with?(".#{File::SEPARATOR}")
-          abs_path = File.expand_path(rux_file)
-          path = abs_path if File.exist?(abs_path)
-        else
-          $LOAD_PATH.each do |lp|
-            check_path = File.expand_path(File.join(lp, rux_file))
+      if File.absolute_path(rux_file) == rux_file && File.exist?(rux_file)
+        path = rux_file
+      elsif rux_file.start_with?(".#{File::SEPARATOR}")
+        abs_path = File.expand_path(rux_file)
+        path = abs_path if File.exist?(abs_path)
+      else
+        $LOAD_PATH.each do |lp|
+          check_path = File.expand_path(File.join(lp, rux_file))
 
-            if File.exist?(check_path)
-              path = check_path
-              break
-            end
+          if File.exist?(check_path)
+            path = check_path
+            break
           end
         end
-
-        raise unless path
-        return false if $LOADED_FEATURES.include?(path)
-
-        # Must call the Kernel.load class method here because that's the one
-        # activesupport doesn't mess with, and in fact the one activesupport
-        # itself uses to actually load files. In case you were curious,
-        # activesupport redefines Object#load and Object#require i.e. the
-        # instance versions that get inherited by all other objects. Yeah,
-        # it's pretty awful stuff.
-        Kernel.load(path)
-        $LOADED_FEATURES << path
-
-        return true
       end
+
+      return super(file) unless path
+      return false if $LOADED_FEATURES.include?(path)
+
+      # Must call the Kernel.load class method here because that's the one
+      # activesupport doesn't mess with, and in fact the one activesupport
+      # itself uses to actually load files. In case you were curious,
+      # activesupport redefines Object#load and Object#require i.e. the
+      # instance versions that get inherited by all other objects. Yeah,
+      # it's pretty awful stuff.
+      Kernel.load(path)
+      $LOADED_FEATURES << path
+
+      return true
     end
   end
 end
